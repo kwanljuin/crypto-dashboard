b7ec760e5b444940bdc0341f734ec38be8eaa02c
Kwan LJuin <kwan.ljuin@gmail.com>
2026-01-21 10:10:32 +0800

    fix: configure Next.js to allow remote images from CoinGecko and integrate the React Compiler Babel plugin

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+        port: '',
+        pathname: '/coins/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
